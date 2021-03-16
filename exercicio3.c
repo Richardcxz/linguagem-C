015156ac43fff9d0215e6cd1299ce621efb25365
Richardcxz <80765748+Richardcxz@users.noreply.github.com>
2021-03-16 16:39:45 -0300

    Add files via upload

---
+++ exercicio3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+int cp;
+float pe, tt;
+
+printf("Informe o preco do produto: ");
+scanf("%f", &pe);
+printf("1 - a vista no dinheiro, 10 porcento de desconto. \n");
+printf("2 - a vista no credito, 5 porcento de desconto. \n");
+printf("3 - 2 vezes sem juros. \n");
+printf("4 - 3 vezes com acrescimo de 10 porcento. \n");
+scanf("%d", &cp);
+switch(cp){
+case 1:
+tt=pe-(pe*0.1);
+printf("O valor a vista sera de R$%f\n", tt);
+break;
+case 2:
+tt=pe-(pe*0.05);
+printf("O valor a vista no credito sera de R$%f\n", tt);
+break;
+case 3:
+tt=pe/2;
+printf("O valor parcelado em duas vezes sem acrescimo sera de R$%f\n", tt);
+break;
+case 4:
+tt=pe+(pe*0.1)/3;
+printf("O valor parcelado em tres vezes com acrescimo de 10 porcento sera de  R$%f\n", tt);
+}
+
+
+
+
+return 0;
+}
