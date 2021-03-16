db3808836e0af703ac9fc587ffafe84de4fc9dc5
Richardcxz <80765748+Richardcxz@users.noreply.github.com>
2021-03-16 15:58:12 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -4,27 +4,24 @@
 
 int main () {
     int resp, raio;
-     float conta;
+float conta;
 
-     printf("Qual o raio da circunferencia: ");
-     scanf("%d%",&raio);
-
-     if(raio>=1){
-     printf("Escolha qual operacao ira fazer: \n");
-     printf("1- Area da circunferencia \n");
-     printf("2- Perimetro da circunferencia \n");
-     scanf("%d",&resp);
-     }
-     switch(resp){
-     case 1:
-     conta=pow(raio,2)*3.14159265359;
-     break;
-     case 2:
-     conta=2*3.14159265359*raio;
-     break;
-     }
-     printf("O resultado da conta e: %f\n", conta);
+printf("Qual o raio da circunferencia: ");
+scanf("%d", &raio);
+printf("Escolha qual operacao ira fazer: \n");
+printf("1- Area da circunferencia \n");
+printf("2- Perimetro da circunferencia \n");
+scanf("%d",&resp);
+switch(resp){
+case 1:
+conta=pow(raio,2)*3.14159265359;
+break;
+case 2:
+conta=2*3.14159265359*raio;
+break;
+}
+printf("O resultado da conta e: %f\n", conta);
 
 
-     return 0;
-     }
+return 0;
+}
