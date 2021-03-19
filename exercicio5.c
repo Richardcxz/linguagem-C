3fb0fff2c641d8a1081a66ccabe4e6d0e282e5bf
Richardcxz <80765748+Richardcxz@users.noreply.github.com>
2021-03-19 19:55:23 -0300

    Add files via upload

---
+++ exercicio5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+int x, cont, novo=0, velho=0;
+
+for(cont=1;cont<=5;cont++){
+printf("Digite uma idade aqui: ");
+scanf("%d", &x);
+if(x>0 && x<21){
+    novo++;
+}
+if(x>50){
+    velho++;
+}}
+    
+if(novo>=0 && velho>=0){
+if(novo>0 && velho>0){
+ printf("O total de pessoas com menos de 21 anos eh: %d\n", novo);
+ printf("O total de pessoas com mais de 50 anos eh: %d\n", velho);
+}
+else if(novo==0 && velho>0){
+ printf("O total de pessoas com menos de 21 anos eh 0.\n");
+ printf("O total de pessoas com mais de 50 anos eh: %d\n", velho);
+}
+else if(novo>0 && velho==0){
+ printf("O total de pessoas com menos de 21 anos eh: %d\n", novo);
+ printf("O total de pessoas com mais de 50 anos eh 0.\n");
+ }
+else if(novo==0 && velho==0){
+    printf("Idade invalida!\n");
+}}
+return 0;
+}
