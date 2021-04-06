ae8606ff992f3bf09b7cf9e40706cd1070f514fd
Richardcxz <80765748+Richardcxz@users.noreply.github.com>
2021-04-06 12:17:58 -0300

    Update ativnova2.c

--- ativnova2.c
+++ ativnova2.c
@@ -4,12 +4,12 @@ int main()
 int v[10],w[10],x,y,xx,yy,z;
 
 for(x=0;x<=9;x++){
-printf("Digite um valor: ");
+printf("Digite um valor para a var V: ");
 scanf("%d",&v[x]);
 xx=xx+v[x];
 }
 for(y=0;y<=9;y++){
-printf("Digite um valor: ");
+printf("Digite um valor para a var W: ");
 scanf("%d",&w[y]);
 yy=yy+w[y];
 }
