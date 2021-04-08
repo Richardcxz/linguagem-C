e1ddf33e890e3ee718cfa9b1d5efd04d666392d2
Richardcxz <80765748+Richardcxz@users.noreply.github.com>
2021-04-08 08:46:37 -0300

    Update ativnovaa1.c

--- ativnovaa1.c
+++ ativnovaa1.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main() {
+int v[10],x[10],z,zz;
+    for(z=0;z<=2;z++){
+    printf("Digite o valor para A: ");
+    scanf("%d",&v[z]);
+    }
+    for(z=0;z<=2;z++){
+    printf("Digite o valor para B: ");
+    scanf("%d",&x[z]);
+    }
+    for(z=0;z<=2;z++){
+    if(v[z]!=x[z]){
+    printf("Uniao: [%d][%d]\n",v[z],x[z]);
+    }
+    else{
+    printf("Uniao: [%d]\n",)
+    }    
+    }
+    return 0;
+}
