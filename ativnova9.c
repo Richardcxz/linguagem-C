f2978e67282324b9a038badd4f24005fbce7d672
Richardcxz <80765748+Richardcxz@users.noreply.github.com>
2021-04-08 08:22:45 -0300

    Update ativnova9.c

--- ativnova9.c
+++ ativnova9.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+
+int main() {
+int v[10],x[10],z,y;
+    for(z=0;z<=9;z++){
+    printf("Digite o valor para A: ");
+    scanf("%d",&v[z]);
+    }
+    for(z=0;z<=9;z++){
+    printf("Valor de A: %d\n",v[z]);
+    }
+    return 0;
+}
