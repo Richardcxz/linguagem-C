288746986c9bd41110a6e27f2514c9575cb5fb58
Richardcxz <80765748+Richardcxz@users.noreply.github.com>
2021-04-29 09:41:53 -0300

    Add files via upload

---
+++ tres.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+void main(){
+char m[50];
+int fin, par, jur, x, y, zz;
+float z;
+
+printf("Modelo do carro:");
+gets(m);
+printf("Valor a ser financiado:");
+scanf("%d",&fin);
+printf("Numero de parcelas:");
+scanf("%d",&par);
+printf("Taxa de juros:");
+scanf("%d",&jur);
+z=jur*0.01;
+zz=fin*z;
+for(x=0;x<=par;x++){
+y=fin+zz;
+}
+x=y/par;
+printf("Valor financiamento do carro modelo %s R$%d\n",m,y);
+printf("Valor de cada uma das %d parcelas: %d\n",par,x);
+}
